0890ce86a0a85c3cf6f06fac104a5ccd432a3a46
Ivan Milivojevic <seltik89@hotmail.com>
2020-12-14 10:47:14 +0100

    added firebase and form hooks

--- src/Containers/NewPost/NewPost.js
+++ src/Containers/NewPost/NewPost.js
@@ -1,73 +1,64 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import Axios from "../../Axios/AxiosUserPosts";
 import styles from "./NewPost.module.css";
 import FormField from "../../Components/UI/FormField";
 
-class NewPost extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      form: [
-        // { name: "author", placeholder: "Author", type: "input", value: "", required: true, valid: false },
-        { name: "title", placeholder: "Post Title", type: "input", value: "", required: true, valid: false },
-        { name: "content", placeholder: "Post Content", type: "textarea", value: "", required: true, valid: false },
-        {
-          name: "category",
-          type: "select",
-          value: "Select Category ...",
-          options: [
-            { value: "Nature", display: "Nature and Life" },
-            { value: "Medicine", display: "Medicine and Chemistry" },
-          ],
-          required: true,
-          valid: false,
-        },
+const NewPost = (props) => {
+  const [formAll, setForm] = useState([
+    { name: "title", placeholder: "Post Title", type: "input", value: "", required: true, valid: false },
+    { name: "content", placeholder: "Post Content", type: "textarea", value: "", required: true, valid: false },
+    {
+      name: "category",
+      type: "select",
+      value: "Select Category ...",
+      options: [
+        { value: "Nature", display: "Nature and Life" },
+        { value: "Medicine", display: "Medicine and Chemistry" },
       ],
-      formSubmitted: false,
-    };
-  }
+      required: true,
+      valid: false,
+    },
+  ]);
+  const [formSubmiting, setFormSubmiting] = useState(false);
 
-  inputValidation = (value) => {
+  const inputValidation = (value) => {
     const inputValue = value.trim();
 
     return inputValue !== "";
   };
 
-  changeValueHandler = (event) => {
+  const changeValueHandler = (event) => {
     const fieldName = event.target.name;
     const fieldValue = event.target.value;
-    const fieldValid = this.inputValidation(fieldValue);
+    const fieldValid = inputValidation(fieldValue);
 
-    this.setState((prevState) => {
-      const updatedForm = [...prevState.form];
+    setForm((prevForm) => {
+      const updatedForm = [...prevForm];
       const updatedField = { ...updatedForm.find((f) => f.name === fieldName) };
       const updatedFieldIndex = updatedForm.findIndex((f) => f.name === fieldName);
       updatedField.value = fieldValue;
       updatedField.valid = fieldValid;
       updatedForm[updatedFieldIndex] = updatedField;
 
-      return {
-        form: updatedForm,
-      };
+      return updatedForm;
     });
   };
 
-  formSubmit = (event) => {
+  const formSubmit = (event) => {
     event.preventDefault();
 
-    if (this.props.isAuthorized) {
-      const shouldSubmit = this.state.form.every((field) => field.valid);
+    if (props.isAuthorized) {
+      const shouldSubmit = formAll.every((field) => field.valid);
 
       if (shouldSubmit) {
         const formData = {};
-        this.state.form.forEach((field) => {
+        formAll.forEach((field) => {
           formData[field.name] = field.value;
         });
 
-        Axios.post(`/posts.json?auth=${this.props.token}`, formData)
+        Axios.post(`/posts.json?auth=${props.token}`, formData)
           .then((res) => {
             console.log(res);
           })
@@ -76,34 +67,27 @@ class NewPost extends Component {
           });
       }
 
-      this.setState({ formSubmitted: true });
+      setFormSubmiting(true);
     } else {
-      this.props.onAuthRedirect("/new-post");
-      this.props.history.push("/auth");
+      props.onAuthRedirect("/new-post");
+      props.history.push("/auth");
     }
   };
 
-  render() {
-    return (
-      <div className={styles.FormWrapper}>
-        <h2>New Post Form:</h2>
-        <form>
-          {this.state.form.map((field) => (
-            <FormField
-              {...field}
-              changed={this.changeValueHandler}
-              key={field.name}
-              checker={this.state.formSubmitted}
-            />
-          ))}
-          <button type="submit" className={styles.SubmitButton} onClick={this.formSubmit}>
-            {this.props.isAuthorized ? "Submit" : "Log In to Submit Post"}
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={styles.FormWrapper}>
+      <h2>New Post Form:</h2>
+      <form>
+        {formAll.map((field) => (
+          <FormField {...field} changed={changeValueHandler} key={field.name} checker={formSubmiting} />
+        ))}
+        <button type="submit" className={styles.SubmitButton} onClick={formSubmit}>
+          {props.isAuthorized ? "Submit" : "Log In to Submit Post"}
+        </button>
+      </form>
+    </div>
+  );
+};
 
 NewPost.propTypes = {
   token: PropTypes.string,
