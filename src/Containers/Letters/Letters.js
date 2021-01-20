889590fef4edc6448a04803a30fd9563d3319f14
Ivan Milivojevic <seltik89@hotmail.com>
2021-01-20 15:26:30 +0100

    feat

--- src/Containers/Letters/Letters.js
+++ src/Containers/Letters/Letters.js
@@ -42,10 +42,6 @@ class Letters extends Component {
     return (
       <div>
         <h2>Hello from Home page</h2>
-        <h2>Hello from Home page</h2>
-        <h4>Hello from Home page new</h4>
-        <h4>Hello from Home page new</h4>
-        <h2>Hello from Home page new 2222</h2>
         <Cockpit change={this.lettersHandler} letters={this.state.letters} />
         {this.state.letters.length ? <CharacterList letters={this.state.letters} clicked={this.removeLetter} /> : null}
       </div>
