74390c9f5222327de362ad3191448bff37563df7
Ivan Milivojevic <seltik89@hotmail.com>
2021-02-25 00:20:24 +0100

    format on save

--- src/Containers/PostsList/Post/Post.js
+++ src/Containers/PostsList/Post/Post.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
 import styles from "./Post.module.css";
 
-const Post = (props) => {
+const Post = props => {
   return (
     <div className={styles.Post} onClick={props.click} onKeyDown={props.click} role="button" tabIndex="0">
       <div>{props.title}</div>
